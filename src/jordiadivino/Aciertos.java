b3dd65ed5559a8110bf5829848172bdff657118e
JordiCL08 <56482626+JordiCL08@users.noreply.github.com>
2019-11-06 22:42:55 +0100

    Update Aciertos.java

--- src/jordiadivino/Aciertos.java
+++ src/jordiadivino/Aciertos.java
@@ -4,7 +4,7 @@ public class Aciertos {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (Math.random()*10+50);
+        int x = (int) (Math.random()*40+10);
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
